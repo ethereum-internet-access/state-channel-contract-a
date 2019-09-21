3ad6ad46db594cb9f9fc5807ef2cee882b6a57d4
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-09-21 12:45:28 +0200

    Literal modification

--- tests/main.test.js
+++ tests/main.test.js
@@ -7,8 +7,8 @@ const WEB3_API = require('web3')
 const WEB3 = new WEB3_API(`http://127.0.0.1:${process.env.GANACHE_PORT}`, null)
 const FS = require('fs')
 
-describe('Token smart contract tests', function () {
-  it('Smart contract symbol should equal IntacTok', async () => {
+describe('State channel contract tests', function () {
+  it('Smart contract symbol should equal StateChannel', async () => {
     const abi = JSON.parse(FS.readFileSync('./contracts/abi.json', 'utf-8'))
     const contract = new WEB3.eth.Contract(abi, process.env.CONTRACT_ADDRESS)
     const accounts = await WEB3.eth.getAccounts()
