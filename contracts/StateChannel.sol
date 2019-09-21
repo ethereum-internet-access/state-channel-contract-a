df8ce365083f7e239c4874a779238091a61fc986
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-09-21 12:34:02 +0200

    Remove unused contract lines

--- contracts/StateChannel.sol
+++ contracts/StateChannel.sol
@@ -8,11 +8,6 @@ contract StateChannel is Ownable {
 
     using SafeMath for uint256;
 
-    mapping (address => uint256) private _balance;
-    mapping (address => uint256) private _lastTokenPayment; // To give the option to revert the last token payment.
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
     /**
        @dev Returns contract name.
     */
